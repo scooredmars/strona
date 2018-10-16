85d6982f4e990faa463eae80bdea4225889e48ce
Olaf Jaworski <olo880@spoko.pl>
2018-10-16 19:38:34 +0200

    zadanie dom

---
+++ static/js/l-3/zadanie-domowe/cz-l-jest-w-m-r.js
@@ -0,0 +1,14 @@
+var a = parseInt(prompt("Podaj liczbe: "));
+
+if (a  < 0)
+{
+    document.write("Liczba jest mniejsza od 0");
+}
+if (a  > 0)
+{
+    document.write("Liczba jest wieksza od 0");
+}
+if (a  == 0)
+{
+    document.write("Liczba jest rowna 0");
+}
---
+++ static/js/l-3/zadanie-domowe/pole-trapezu.js
@@ -0,0 +1,8 @@
+var a = parseInt(prompt("Podaj 1 liczbe: "));
+var b = parseInt(prompt("Podaj 2 liczbe: "));
+var h = parseInt(prompt("Podaj 3 liczbe: "));
+var x;
+
+x=((a+b)*h)/2
+
+document.write("Pole trapezu = "+x)
